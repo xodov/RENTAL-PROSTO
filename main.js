7a1092a373971fdb1bfe6a0a36c5386f74659963
Дмитрий <admin@MBP-Dmitrij.Dlink>
2024-10-13 16:14:34 +0300

    active buttons

--- main.js
+++ main.js
@@ -2,13 +2,51 @@ var myImage = document.getElementById("main1");
 var imageArray = ["images/bg.jpg", "images/bg2.jpg", "images/bg3.jpg", "images/bg4.jpg", "images/bg5.jpg", "images/bg6.jpg"];
 var imageIndex = 0;
 
-
 function changeImage() {
   myImage.style.backgroundImage = 'url(' + imageArray[imageIndex] + ')';
   imageIndex++;
   if (imageIndex >= imageArray.length) {
     imageIndex = 0;
   }
+
+}
+var timeChange = setInterval(changeImage, 5000)
+
+
+var button = document.getElementById('btn1');
+
+function buttonChange1() {
+  if (button.onclick) {
+    myImage.style.backgroundImage = "url('images/bg.jpg')"
+  }
 }
 
-var timeChange = setInterval(changeImage, 2500)
+function buttonChange2() {
+  if (button.onclick) {
+    myImage.style.backgroundImage = "url('images/bg2.jpg')"
+  }
+}
+
+function buttonChange3() {
+  if (button.onclick) {
+    myImage.style.backgroundImage = "url('images/bg3.jpg')"
+  }
+}
+
+function buttonChange4() {
+  if (button.onclick) {
+    myImage.style.backgroundImage = "url('images/bg4.jpg')"
+  }
+}
+
+function buttonChange5() {
+  if (button.onclick) {
+    myImage.style.backgroundImage = "url('images/bg5.jpg')"
+  }
+}
+
+function buttonChange6() {
+  if (button.onclick) {
+    myImage.style.backgroundImage = "url('images/bg6.jpg')"
+  }
+}
